fa7483e8fc024f45537020e7ca94ccb1d16c83d1
lycilph <lycilph@gmail.com>
2024-10-04 09:02:56 +0200

    Add project files.

---
+++ VonNeumannIncremental/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+
+// Check out BigDouble for the numbers
+
+namespace VonNeumannIncremental;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
